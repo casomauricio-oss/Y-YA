5786ebf52cfb94ab4724b5929178155315393b95
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-10 15:41:23 -0500

    Formulario para el registro de usuarios nuevos en YÁYA

--- views/formRegisterUsers.php
+++ views/formRegisterUsers.php
@@ -1,30 +1,47 @@
 <div class="content">
-    <div class="info">
-        <h5>Registra un nuevo Usuario YÁYA</h5>
+    <div class="title-info">
+        <h5>Registra un nuevo usuario en YÁYA</h5>
     </div>
     <!-- usuario -->
     <div class="form-register">
         <form method="POST" action="/security/validacionRgistroUsuario.php">
-            <input name="nombreUsuario" type="text" value="" placeholder="Ingresar Nombre" required>
-            <input name="apellido" type="text" value="" placeholder="Ingresar Apellidos" required>
-            <input name="numeroCedula" type="text" value="" placeholder="Ingrese numero de Cedula" required>
-            <input name="FechaNacimiento" type="date" value="" placeholder="Ingrese fecha de nacimiento" required>
-            <input name="fechaRegistro" type="hidden" value="<?php echo date('Y-m-d H:i:s'); ?>">
+            <div class="infoUser">
+                <label for="nombreUsuario">Nombre de usuario:</label>
+                <input name="nombreUsuario" type="text" value="" placeholder="Ingresar Nombre" required>
+                <label for="apellido">Apellidos:</label>
+                <input name="apellido" type="text" value="" placeholder="Ingresar Apellidos" required>
+                <label for="numeroCedula">Número de Cédula:</label>
+                <input name="numeroCedula" type="text" value="" placeholder="Ingrese numero de Cedula" required>
+                <label for="FechaNacimiento">Fecha de Nacimiento:</label>
+                <input name="FechaNacimiento" type="date" value="" placeholder="Ingrese fecha de nacimiento" required>
+            </div>
 
-            <!-- rol -->
-            <input name="rol" type="text" value="" placeholder="seleccione rol" required>
+            <label for="fechaRegistro">Fecha de Registro:</label>
+            <div class="infoUserSec">
+                <input name="fechaRegistro" type="hidden" value="<?php echo date('Y-m-d H:i:s'); ?>">
 
-            <!-- credenciales -->
-            <input name="email" type="email" value="" placeholder="ingrese email" required>
-            <input name="contraseña" type="password" value="" placeholder="ingrese Contraseña" required>
+                <!-- rol -->
+                <label for="rol">Rol:</label>
+                <input name="rol" type="text" value="" placeholder="seleccione rol" required>
 
-            <!-- contactos-->
-            <input name="telefono" type="text" value="" placeholder=" numero de telefono" required>
-            <input name="email_contacto" type="email" value="" placeholder="ingrese email de contacto" required>
+                <!-- credenciales -->
+                <label for="email">Email:</label>
+                <input name="email" type="email" value="" placeholder="ingrese email" required>
+                <label for="contraseña">Contraseña:</label>
+                <input name="contraseña" type="password" value="" placeholder="ingrese Contraseña" required>
 
-            <!-- Direcion -->
-            <input name="direccion" type="text" value="" placeholder="ingrese direccion" required>
-            <input name="Guardar" type="submit" value="Guardar">
+                <!-- contactos-->
+                <label for="telefono">Teléfono:</label>
+
+                <input name="telefono" type="text" value="" placeholder=" numero de telefono" required>
+                <label for="email_contacto">Email de contacto:</label>
+                <input name="email_contacto" type="email" value="" placeholder="ingrese email de contacto" required>
+
+                <!-- Direcion -->
+                <label for="direccion">Dirección:</label>
+                <input name="direccion" type="text" value="" placeholder="ingrese direccion" required>
+                <input name="Guardar" type="submit" value="Guardar">
+            </div>
         </form>
     </div>
 </div>
