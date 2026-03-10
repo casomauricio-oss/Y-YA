b9610dacb30be71a4ab1e140ccaa661d0151ac55
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-10 14:31:12 -0500

    En el encabezado se importa el contenido por defecto por si no esta definido, se importan los links de los archivos de estilos para cada componente, en el body se incluye el navbar seguido de el contenedor principal que contiene el sidebar.php y la varibale contenido que es la q almacena el contenido dinamico

--- views/home.php
+++ views/home.php
@@ -1,5 +1,8 @@
 <?php 
-    $contenido = "./views/dashboard.php";
+    // Define el contenido por defecto si no está definido
+    if (!isset($contenido)) {
+        $contenido = "dashboard.php";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -9,27 +12,26 @@
     <meta charset="UTF-8">
     <title>Mi Dashboard</title>
     <!-- Vincula los archivos de estilo -->
-    <link rel="stylesheet" type="text/css" media="screen" href="./public/css/style.css"> <!-- Estilos generales -->
-    <link rel="stylesheet" type="text/css" media="screen" href="./public/css/navbar.css"> <!-- Estilos para la barra de navegación -->
-    <link rel="stylesheet" type="text/css" media="screen" href="./public/css/sidebar.css"> <!-- Estilos para la barra lateral -->
-    <link rel="stylesheet" type="text/css" media="screen" href="./public/css/content.css"> <!-- Estilos para el contenido principal -->
-    <link rel="stylesheet" type="text/css" media="screen" href="./public/css/container.css"> <!-- Estilos para el contenedor principal -->
-    <link rel="stylesheet" type="text/css" media="screen" href="./public/css/footer.css"> <!-- Estilos para el pie de página -->
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/style.css"> <!-- Estilos generales -->
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/navbar.css"> <!-- Estilos para la barra de navegación -->
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/sidebar.css"> <!-- Estilos para la barra lateral -->
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/content.css"> <!-- Estilos para el contenido principal -->
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/container.css"> <!-- Estilos para el contenedor principal -->
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/footer.css"> <!-- Estilos para el pie de página -->
 </head>
 
 <body>
     <?php
-    include './views/navbar.php'; ?>
+    include __DIR__ . '/navbar.php'; ?>
 
     <!-- CONTENEDOR PRINCIPAL -->
     <div class="container">
         <!-- Incluye el sidebar -->
-        <?php include 'sidebar.php';
-        //Area de contenido principal
-        include_once $contenido;
-        ?>
+        <?php include __DIR__ . '/sidebar.php'; ?>
+        <!-- Area de contenido principal -->
+        <?php include_once __DIR__ . '/' . $contenido; ?>
     </div>
-    <?php include './views/footer.php'; ?>
+    <?php include __DIR__ . '/footer.php'; ?>
 </body>
 
 </html>
